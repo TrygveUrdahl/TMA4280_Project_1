f53e6de2b27cf0d22d5f707a5b09009c1c0d61cf
Trygve Scheline Urdahl <trygve.urdahl@hotmail.com>
2019-02-20 20:55:40 +0100

    Added MPI to CMakeList

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include <mpi.h>
 
 int main(int argc, char** argv) {
+  MPI_Init(&argc, &argv);
 
+  int rank, size;
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::cout << "Hello from rank " << rank << " of " << size << "." << std::endl;
+
+  MPI_Finalize();
 }
